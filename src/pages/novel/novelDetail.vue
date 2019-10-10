34c4caf0b7ac1ffd4a2c71bbae8219ce1d573a2b
wisteria-Lynn <54693592+wisteria-Lynn@users.noreply.github.com>
2019-10-10 14:20:43 +0800

    小说啦

--- src/pages/novel/novelDetail.vue
+++ src/pages/novel/novelDetail.vue
@@ -1,64 +1,200 @@
 <template>
-    <div class="novelDetail">
-		<div class="panel">
-			<div class="novelTitle t-center">{{novelInfo.title}}</div>
-			<div class="novelTxt" v-html="html">
+    <div class="novelIndex novelDetail">
+		<div class="slideTip" @click="getSliceMenu">
+			<i class="iconfont icon1"></i>
+		</div>
+		<template v-if="isSlideMenu">
+			<div class="headSlideMenu" :class="[isSlideMenuBlock?'menu-show':'menu-hidden']" @click="hideSlideNav">
+				<div class="headSlideMenuChild">
+					<span>{{novelTitle}}</span>
+					<template v-for="(item,index) in sideData">
+						<!--<router-link :to="{path:item.url}" :class="[{'is-active':item.url === $route.path}]">{{item.title}}</router-link>-->
+						<span @click="getNovelSpce(index)" :class="[{'is-active':index=== num}]">第{{index+1}}章&nbsp;&nbsp;{{item.title}}</span>
+					</template>
+				</div>
 			</div>
+		</template>
+		<div class="panel">
+			<div class="panel-card novel-bg">
+				<div class="panel-card-item novel-arc">
+					<h3 class="novelTitle t-center">第{{num+1}}章&nbsp;&nbsp;{{title}}</h3>
+					<div class="novelTxt" v-html="html">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="novelBtn">
+			<el-button class="btn btn-success" @click="getNext('up')">上一章</el-button>
+			<el-button class="btn btn-success" @click="back()">返回书架</el-button>
+			<el-button class="btn btn-success" @click="getNext('down')">下一章</el-button>
 		</div>
 	</div>
 </template>
 
 <script>
-	import {novel} from '../../../static/js/novel-1001'
+	import {getQueryString} from "../../util/methods"
     export default {
+		inject: ['reload'],
         name: "novelDetail",
 		data(){
         	return{
-        		novelInfo:novel[0],
-				html:''
+        		novelTitle:'',
+        		title:'',
+				html:'',
+				num:0,
+				sideData:[],
+				isSlideMenu:false,
+				isSlideMenuBlock: false,
+			}
+		},
+		methods:{
+			getSliceMenu() {
+				this.isSlideMenu = true
+				this.isSlideMenuBlock = !this.isSlideMenuBlock
+			},
+			hideSlideNav(e){
+				let sp2 = document.getElementsByClassName('headSlideMenuChild')[0]
+				if (sp2) {
+					// 点击其他地方 导航隐藏
+					if (!sp2.contains(e.target)) {
+						this.isSlideMenuBlock = false
+					}
+				}
+			},
+			getNovelSpce(index){
+				this.$router.push({
+					path:'/novel/novelDetail',
+					query:{
+						id:this.$route.query.id,
+						num:index,
+						novelTitle:this.novelTitle
+					}
+				})
+			},
+			getNext(str){
+				if(str === 'up' && this.num === 0){
+					this.layer.msg('已经是第1章啦！')
+					return false
+				}
+				if(str === 'down' && this.num === novelArc.novel.length-1){
+					this.layer.msg('已经是最后一章啦！')
+					return false
+				}
+				let index = str === 'up' ? this.num - 1 : this.num + 1
+				this.getNovelSpce(index)
+			},
+			back(){
+				this.$router.push({path:'/novel'})
 			}
 		},
 		created(){
-        	let txt= novel[0].txt.split('\n\n')
+			let novelArc
+			switch (Number(getQueryString('id'))) {
+				case 1001:
+					novelArc = require('../../../static/js/novel/novel-1001')
+					break;
+				case 1002:
+					novelArc = require('../../../static/js/novel/novel-1002')
+					break;
+				case 1003:
+					novelArc = require('../../../static/js/novel/novel-1003')
+					break;
+				case 1004:
+					novelArc = require('../../../static/js/novel/novel-1004')
+					break;
+			}
+			this.sideData = novelArc.novel
+			this.num = Number(this.$route.query.num)
+			this.novelTitle = this.$route.query.novelTitle
+			let txt= novelArc.novel[this.num].txt.split('\n')
+			this.title = novelArc.novel[this.num].title
 			this.html = ''
 			for(let i = 0;i<txt.length;i++){
-				this.html+=`<p>${txt[i]}</p>`
+				this.html+=`<p style="margin:20px 0;font-size:16px;">${txt[i]}</p>`
 			}
 		}
     }
 </script>
-
-<style lang="less">
-	.panel{
-		min-height:300px;
-		width:95%;
-		margin:20px 5%;
-		background:#fff;
-		.novelTitle{
-			padding-top:30px;
-			font-size:20px;
-			font-weight: 600;
+<style scoped lang="less">
+	@import "../../assets/less/novel";
+	.slideTip{
+		display: none;
+		position: fixed;
+		top: 50%;
+		left: 2%;
+		width: 40px;
+		height: 40px;
+		background: rgba(96, 122, 175,.5);
+		color: #fff;
+		border-radius: 25px;
+		line-height: 40px;
+		text-align: center;
+		cursor: pointer;
+		z-index:900;
+		i{
+			font-size:22px;
 		}
-		.novelTxt{
-			text-indent:1.5em;
-			margin:auto;
-			p{
-				margin:20px;
+	}
+	.headSlideMenu {
+		position: fixed;
+		top:0;
+		width: 100%;
+		height: 100%;
+		left:-100%;
+		background: rgba(0, 0, 0, 0.1);
+		z-index: 1000;
+		&.menu-show {
+			-webkit-animation: menuNavShow .5s forwards;
+			animation: menuNavShow .5s forwards;
+		}
+
+		&.menu-hidden {
+			-webkit-animation: menuNavHidden .5s forwards;
+			animation: menuNavHidden .5s forwards;
+		}
+
+		.headSlideMenuChild {
+			width:60%;
+			height:100%;
+			/*margin-top:62px;*/
+			padding-top: 10px;
+			background: #fff;
+			span{
+				display: block;
+				color:#333;
+				padding-left:20px;
+				line-height: 40px;
+				&:hover{
+					color:#246dff;
+					background:#ccc;
+				}
+				&.is-active{
+					color:#246dff;
+					background:#ccc;
+				}
 			}
 		}
 	}
 	@media (max-width: 768px) {
-		.panel {
-			width:100%;
-			margin:20px 0;
-		}
-		.novelTxt{
-			width:100%;
+		.slideTip {
+			display: block !important;
 		}
 	}
-	@media (min-width: 982px) {
-		.novelTxt{
-			width:80%;
+	@keyframes menuNavShow {
+		from {
+			left: -100%;
+		}
+		to {
+			left: 0;
+		}
+	}
+
+	@keyframes menuNavHidden {
+		from {
+			left: 0;
+		}
+		to {
+			left: -100%;
 		}
 	}
 </style>
--- src/pages/novel/novelIndex.vue
+++ src/pages/novel/novelIndex.vue
@@ -2,13 +2,17 @@
     <div class="novelIndex">
 		<headerOne></headerOne>
 		<div class="panel" v-if="$route.path.split('/').length <= 2">
-			<div class="novelWrap" v-for="item in novelList">
-				<img :src="item.img" @click="toReadIt(item.id)">
-				<span>{{item.title}}</span>
-				<div>{{item.des}}</div>
+			<div class="panel-card">
+				<div class="panel-card-item novel-list" v-for="item in novelList">
+					<img :src="item.img" @click="toReadIt(item.id,item.title)">
+					<div>
+						<h4>{{item.title}}</h4>
+						<span>简介：{{item.des}}</span>
+					</div>
+				</div>
 			</div>
 		</div>
-		<router-view></router-view>
+		<router-view :key="$route.fullPath"></router-view>
 	</div>
 </template>
 
@@ -19,20 +23,40 @@
         	return{
 				novelList:[
 					{
-						title:'解祤忧',
+						title:'解语歌·书绝天下，泪断成殇',
 						img:require('../../assets/img/novel/novel-1.png'),
-						id:0,
+						id:1001,
 						des:'他一生'
+					},
+					{
+						title:'解语歌·碧玉燎原，风中潇潇',
+						img:require('../../assets/img/novel/novel-1.png'),
+						id:1002,
+						des:'他一生'
+					},
+					{
+						title:'解语歌·宿命缘劫，浮生何归',
+						img:require('../../assets/img/novel/novel-1.png'),
+						id:1003,
+						des:'他一生'
+					},
+					{
+						title:'渔歌唱晚',
+						img:require('../../assets/img/novel/novel-1.png'),
+						id:1004,
+						des:'小渔，阿战'
 					}
 				]
 			}
 		},
 		methods:{
-			toReadIt(id){
+			toReadIt(id,title){
 				this.$router.push({
 					path:'/novel/novelDetail',
 					query:{
-						id:id
+						id:id,
+						num:'0',
+						novelTitle:title
 					}
 				})
 			}
@@ -41,30 +65,5 @@
 </script>
 
 <style scoped lang="less">
-	.novelIndex{
-		.novelWrap{
-			margin:20px;
-			img{
-				height:100px;
-				cursor: pointer;
-			}
-			span{
-				display: block;
-			}
-		}
-	}
-</style>
-<style lang="less">
-	.panel{
-		min-height:300px;
-		width:95%;
-		margin:20px 5%;
-		background:#fff;
-	}
-	@media (max-width: 768px) {
-		.panel {
-			width:100%;
-			margin:20px 0;
-		}
-	}
+	@import "../../assets/less/novel";
 </style>
