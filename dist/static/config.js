0336b64c580bc6b6bbac138786a39de28c691512
wisteria-Lynn <54693592+wisteria-Lynn@users.noreply.github.com>
2020-05-22 16:06:38 +0800

    删除dist

--- dist/static/config.js
+++
@@ -1,6 +0,0 @@
-// 全局变量
-const dataApi = process.env.NODE_ENV === 'development'
-const dataApi2 = '0990'
-const novel = []
-const avatar = 'https://wisteria-lynn.github.io/public/img/avatar.jpg'
-export default { dataApi, dataApi2, novel, avatar }
